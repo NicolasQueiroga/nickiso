aafeb383c11ee9b8579656a6754f119d229ea1f6
Nicolas Queiroga <nicolasqueiroga@me.com>
2025-07-08 02:36:25 +0000

    add screen_setup.sh for display configuration setup

---
+++ screen_setup.sh
@@ -0,0 +1,1 @@
+xrandr --output DP-0 --primary --mode 2560x1080 --rate 144 --pos 1839x1615 --rotate normal --output DP-1 --off --output DP-2 --mode 1920x1080 --rate 240 --pos 759x1195 --rotate left --output DP-3 --off --output HDMI-0 --off --output DP-4 --off --output DP-5 --off --output HDMI-1-1 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off
